b3cf476411265e3a2db1b6bd452bde5d757bdeec
afanc0 <gianromani.gr@gmail.com>
2024-09-11 03:18:26 -0400

    chore(folders): added folder structure for lesson post

--- src/features/dashboard-content/src/Dashboard-Content.tsx
+++ src/features/dashboard-content/src/Dashboard-Content.tsx
@@ -1,23 +1,25 @@
-import LessonCard from "./components/LessonCard";
+import LessonCard from "./components/LessonCard"
 import style from "./css/dashboardcontent.module.scss"
+import lessonData from "../../../data-lesson.json"
 
 interface DashboardContentProps {
-    refVal?: React.RefObject<HTMLDivElement>;
+    refVal?: React.RefObject<HTMLDivElement>
 }
 
 function DashboardContent(props: DashboardContentProps) {
 
     const value = props.refVal
 
+    const renderLessonCard = () => {
+        return Object.keys(lessonData).map((key) => (
+            <LessonCard lessonId={key as keyof typeof lessonData} />
+        ))
+    }
+
     return (
         <section ref={value} id={style["dasboard-content-white"]} className={`${style["x-space-padding-96"]}`}>
             <div className={`${style["flex-wrap"]} ${style["y-space-padding-72"]} ${style["flex"]} ${style["justify-center"]} ${style["gap-50"]}`}>
-                <LessonCard lessonId="1"/> 
-                <LessonCard lessonId="2"/>
-                <LessonCard lessonId="5"/>
-                <LessonCard lessonId="3"/> 
-                <LessonCard lessonId="4"/> 
-                <LessonCard lessonId="6"/> 
+                {renderLessonCard()}
             </div>
         </section>
     )
