52a8349cec012860776b4247670316b6aa1ad18f
afanc0 <gianromani.gr@gmail.com>
2024-09-12 22:50:38 -0400

    feat(router): setup browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import App from "./App.tsx";
+import LessonPost from "./pages/LessonPost/index.ts";
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <BrowserRouter basename="/vslib-tutor-app/">
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="/:id/:title" element={<LessonPost />} />
+    </Routes>
+  </BrowserRouter>
 )
