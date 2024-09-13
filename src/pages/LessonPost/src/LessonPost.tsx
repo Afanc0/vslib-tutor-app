fbf082a0228f486dafd1c4cb62afeb3832b1c60e
afanc0 <gianromani.gr@gmail.com>
2024-09-12 22:51:24 -0400

    feat(window): reset window upon loading

--- src/pages/LessonPost/src/LessonPost.tsx
+++ src/pages/LessonPost/src/LessonPost.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { Navbar, Footer } from "./components";
 import ContentLayout from "./features/Content-Layout";
 
 function LessonPost() {
+
+    useEffect(()=>{
+        window.scrollTo(0, 0);
+    },[])
+
     return (
         <>
             <Navbar />
