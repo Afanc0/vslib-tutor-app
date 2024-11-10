ec212ab8634887732e769da6f573c871ecee50af
afanc0 <gianromani.gr@gmail.com>
2024-11-10 03:25:12 -0500

    fix(navbar): link to home from navbar fixed

--- src/pages/LessonPost/src/components/Navbar/Navbar.tsx
+++ src/pages/LessonPost/src/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
         <header id={styles["header-section"]} >
             <div className={`${styles["x-space-padding-96"]} ${styles["y-space-padding-32"]}`}>
                 <div className={`${styles["flex"]} ${styles["align-center"]} ${styles["w-75"]} ${styles["justify-space-between"]}`}>
-                    <a href={"/"}>
+                    <a href={"/vslib-tutor-app"}>
                         <div className={`${styles["flex"]} ${styles["align-center"]} ${styles["gap-8"]}`}>
                                 <TitleIcon />
                             <span className={`${styles["text-size-16"]} ${styles["semibold-text"]}`}>LEFT4SCRIPT</span>
